12c63eccdd48a83863aa614cd78edb816d02ca85
Han <hwl784770@outlook.com>
2023-09-05 10:30:10 +0800

    新增Member会员模块

--- member/src/main/java/com/han/train/member/controller/TestController.java
+++ member/src/main/java/com/han/train/member/controller/TestController.java
@@ -1,4 +1,4 @@
-package com.han.train;
+package com.han.train.member.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
