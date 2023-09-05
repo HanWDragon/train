c8a3b0529e28e46c60f4e3f9d5f95b3ec300d38d
Han <hwl784770@outlook.com>
2023-09-05 23:06:29 +0800

    添加了Mybatis代码生成器，并且连接数据库成功

--- member/src/main/java/com/han/train/member/service/MemberService.java
+++ member/src/main/java/com/han/train/member/service/MemberService.java
@@ -16,7 +16,7 @@ public class MemberService {
     private MemberMapper memberMapper;
 
     public int count() {
-        return memberMapper.count();
+        return Math.toIntExact(memberMapper.countByExample(null));
     }
 
 }
