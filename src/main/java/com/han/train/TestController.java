7125db55bda9786853fc6bfd479179a7a2f59820
Han <hwl784770@outlook.com>
2023-09-05 09:08:25 +0800

    初始化

---
+++ src/main/java/com/han/train/TestController.java
@@ -0,0 +1,17 @@
+package com.han.train;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Han
+ * @date 2023年09月05日 08:57
+ */
+@RestController
+public class TestController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World";
+    }
+
+}
