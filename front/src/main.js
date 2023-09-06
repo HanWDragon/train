c274b02970e95065a5245aa527b9755b73f7284d
Han <hwl784770@outlook.com>
2023-09-06 15:08:50 +0800

    集成Ant Design及其图标

--- front/src/main.js
+++ front/src/main.js
@@ -2,5 +2,17 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import * as Icons from '@ant-design/icons-vue'
 
-createApp(App).use(store).use(router).mount('#app')
+// 引入Ant Design
+import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/reset.css';
+
+const app = createApp(App)
+app.use(Antd).use(store).use(router).mount('#app')
+
+// 全局使用图标
+const icons = Icons
+for (let iconsKey in icons) {
+    app.component(iconsKey,icons[iconsKey])
+}
