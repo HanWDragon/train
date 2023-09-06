ee0b578b709857954a4ee4a141834b9fd05cc753
Han <hwl784770@outlook.com>
2023-09-06 10:51:36 +0800

    使用自定义异常处理异常业务，不写入堆栈信息，提高性能

--- common/src/main/java/com/han/train/common/exception/BusinessException.java
+++ common/src/main/java/com/han/train/common/exception/BusinessException.java
@@ -27,4 +27,13 @@ public class BusinessException extends RuntimeException{
     public void setE(BusinessExceptionEnum e) {
         this.e = e;
     }
+
+    /**
+     * 不写入堆栈信息，提高性能
+     * @return 直接返回最外层异常
+     */
+    @Override
+    public synchronized Throwable fillInStackTrace() {
+        return this;
+    }
 }
