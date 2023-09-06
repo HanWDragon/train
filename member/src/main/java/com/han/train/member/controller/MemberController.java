18b2ffb3fed726a48a9c0c25e5ec1a3fc15de1d2
Han <hwl784770@outlook.com>
2023-09-06 09:32:47 +0800

    实现了一个简单的注册接口，并且书写了测试

--- member/src/main/java/com/han/train/member/controller/MemberController.java
+++ member/src/main/java/com/han/train/member/controller/MemberController.java
@@ -2,9 +2,7 @@ package com.han.train.member.controller;
 
 import com.han.train.member.service.MemberService;
 import jakarta.annotation.Resource;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author Han
@@ -20,6 +18,11 @@ public class MemberController {
     @GetMapping("/count")
     public int count() {
         return memberService.count();
+    }
+
+    @PostMapping("/register")
+    public long register(String mobile) {
+        return memberService.register(mobile);
     }
 
 }
--- member/src/main/java/com/han/train/member/service/MemberService.java
+++ member/src/main/java/com/han/train/member/service/MemberService.java
@@ -1,8 +1,13 @@
 package com.han.train.member.service;
 
+import cn.hutool.core.collection.CollUtil;
+import com.han.train.member.domain.Member;
+import com.han.train.member.domain.MemberExample;
 import com.han.train.member.mapper.MemberMapper;
 import jakarta.annotation.Resource;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * @author Han
@@ -17,6 +22,27 @@ public class MemberService {
 
     public int count() {
         return Math.toIntExact(memberMapper.countByExample(null));
+    }
+
+
+    public long register(String mobile) {
+        MemberExample memberExample = new MemberExample();
+        memberExample.createCriteria().andMobileEqualTo(mobile);
+        List<Member> members = memberMapper.selectByExample(memberExample);
+        // 带验证码的注册可以用这种方式，有验证码，说明手机号是本人用，原来注册过的，就不需要保存了，直接登录
+        // 也就是这个接口块，既能注册又能登录
+        if (CollUtil.isNotEmpty(members)) {
+            // 这是一种做法，还有一种做法是抛出异常中断业务
+//            return members.get(0).getId();
+            throw new RuntimeException("手机号已注册");
+        }
+
+
+        Member member = new Member();
+        member.setId(System.currentTimeMillis());
+        member.setMobile(mobile);
+        memberMapper.insert(member);
+        return member.getId();
     }
 
 }
