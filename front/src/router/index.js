108ac32ff3281b5260c23cbd4fcf1ee48e171f1c
Han <hwl784770@outlook.com>
2023-12-21 16:29:50 +0800

    增加前端路由登录拦截，访问所有的控制台页面都需要登录

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -1,7 +1,8 @@
 import {createRouter, createWebHistory} from 'vue-router'
+import {notification} from "ant-design-vue";
+import store from "@/store";
 
 const routes = [
-
     {
         path: '/login',
         name: 'login',
@@ -16,7 +17,12 @@ const routes = [
         // route level code-splitting
         // this generates a separate chunk (about.[hash].js) for this route
         // which is lazy-loaded when the route is visited.
-        component: () => import('../views/MainView.vue')
+        component: () => import('../views/MainView.vue'),
+
+        // 添加元数据，在前端进行拦截，而axios是基于后端返回的信息进行验证
+        meta: {
+            loginRequire: true
+        }
     }
 ]
 
@@ -24,5 +30,26 @@ const router = createRouter({
     history: createWebHistory(process.env.BASE_URL),
     routes
 })
+
+// 路由登录拦截,在每次跳转之前进行检查 from ——> to  而 next 就是你要执行的操作
+router.beforeEach((to, from, next) => {
+    // 要不要对meta.loginRequire属性做监控拦截
+    if (to.matched.some(item => {
+        console.log(item, "是否需要登录校验：", item.meta.loginRequire || false);
+        return item.meta.loginRequire;
+    })) {
+        const _member = store.state.member;
+        console.log("页面登录校验开始：", _member);
+        if (!_member.token) {
+            console.log("用户未登录或登录超时！");
+            notification.error({message: "未登录或登录超时"});
+            next('/login');
+        } else {
+            next();
+        }
+    } else {
+        next();
+    }
+});
 
 export default router
