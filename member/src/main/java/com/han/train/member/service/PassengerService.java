30213a13fc3101dcccf5cb3085686fcbdcc685a5
Han <hwl784770@outlook.com>
2023-12-27 15:22:55 +0800

    封装分页返回结果

--- member/src/main/java/com/han/train/member/service/PassengerService.java
+++ member/src/main/java/com/han/train/member/service/PassengerService.java
@@ -4,7 +4,9 @@ import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.date.DateTime;
 import cn.hutool.core.util.ObjectUtil;
 import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.han.train.common.context.LoginMemberContext;
+import com.han.train.common.response.PageResp;
 import com.han.train.common.util.SnowUtil;
 import com.han.train.member.domain.Passenger;
 import com.han.train.member.domain.PassengerExample;
@@ -13,6 +15,8 @@ import com.han.train.member.requset.PassengerQueryReq;
 import com.han.train.member.requset.PassengerSaveReq;
 import com.han.train.member.response.PassengerQuerryResp;
 import jakarta.annotation.Resource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -24,6 +28,7 @@ import java.util.List;
 @Service
 public class PassengerService {
 
+    private static final Logger LOG = LoggerFactory.getLogger(PassengerService.class);
     @Resource
     private PassengerMapper passengerMapper;
 
@@ -40,16 +45,28 @@ public class PassengerService {
 
     // service 层通常是要做的比较通用，controller比较细分，需要接口隔离
     // 返回的实体一般不实用domain包下的实体，而是自己创建一个方便修改
-    public List<PassengerQuerryResp> queryList(PassengerQueryReq req) {
+    public PageResp<PassengerQuerryResp> queryList(PassengerQueryReq req) {
         PassengerExample passengerExample = new PassengerExample();
         PassengerExample.Criteria criteria = passengerExample.createCriteria();
         if (ObjectUtil.isNotNull(req.getMemberId())) {
             criteria.andMemberIdEqualTo(req.getMemberId());
         }
+
+        LOG.info("查询页码：{}", req.getPage());
+        LOG.info("每页条数：{}", req.getSize());
         // 一般将分页和查询放在一起，因为这个是一次性的
         PageHelper.startPage(req.getPage(), req.getSize());
         List<Passenger> passengerList = passengerMapper.selectByExample(passengerExample);
-        return BeanUtil.copyToList(passengerList, PassengerQuerryResp.class);
+        PageInfo<Passenger> pageInfo = new PageInfo<>(passengerList);
+        LOG.info("总行数：{}", pageInfo.getTotal());
+        LOG.info("总页数：{}", pageInfo.getPages());
+
+        List<PassengerQuerryResp> list = BeanUtil.copyToList(passengerList, PassengerQuerryResp.class);
+
+        PageResp<PassengerQuerryResp> pageResp = new PageResp<>();
+        pageResp.setTotal(pageInfo.getTotal());
+        pageResp.setList(list);
+        return pageResp;
     }
 
 }
