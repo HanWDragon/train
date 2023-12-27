10000d3803d76720642d3b4657b27cae5d06823c
Han <hwl784770@outlook.com>
2023-12-27 15:25:34 +0800

    格式化返回的日期字段

--- member/src/main/java/com/han/train/member/response/PassengerQuerryResp.java
+++ member/src/main/java/com/han/train/member/response/PassengerQuerryResp.java
@@ -1,5 +1,7 @@
 package com.han.train.member.response;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import java.util.Date;
 
 public class PassengerQuerryResp {
@@ -13,8 +15,10 @@ public class PassengerQuerryResp {
 
     private String type;
 
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
     private Date createTime;
 
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
     private Date updateTime;
 
     public Long getId() {
