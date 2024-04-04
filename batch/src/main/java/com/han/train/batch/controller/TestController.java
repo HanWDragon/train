99906b414dec026afe5773c1f8a3c1c1b5f2fbde
Haan <hwl784770@outlook.com>
2024-04-04 14:19:00 +0800

    集成openfeigh,实现服务间调用。给每个应用增加应用名，可搭配配置中心使用

--- batch/src/main/java/com/han/train/batch/controller/TestController.java
+++ batch/src/main/java/com/han/train/batch/controller/TestController.java
@@ -1,5 +1,9 @@
 package com.han.train.batch.controller;
 
+import com.han.train.batch.feign.BusinessFeign;
+import jakarta.annotation.Resource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,9 +13,17 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class TestController {
+
+    private static final Logger LOG = LoggerFactory.getLogger(TestController.class);
+
+    @Resource
+    BusinessFeign businessFeign;
+
     @GetMapping("/hello")
     public String hello() {
-        return "Hello World Batch";
+        String businessHello = businessFeign.hello();
+        LOG.info(businessHello);
+        return "Hello World! Batch! " + businessHello;
     }
 
 }
