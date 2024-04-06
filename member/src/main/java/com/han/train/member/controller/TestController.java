f2bb4041bbc25c21a62fb6f4a9f9fd9146283cbb
Haan <hwl784770@outlook.com>
2024-04-07 07:53:02 +0800

    集成Nacos配置中心，实现动态刷新功能

--- member/src/main/java/com/han/train/member/controller/TestController.java
+++ member/src/main/java/com/han/train/member/controller/TestController.java
@@ -1,5 +1,7 @@
 package com.han.train.member.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,10 +10,16 @@ import org.springframework.web.bind.annotation.RestController;
  * @date 2023年09月05日 08:57
  */
 @RestController
+@RefreshScope
 public class TestController {
+
+    @Value("${test.nacos}")
+    private String test;
+
+
     @GetMapping("/hello")
     public String hello() {
-        return "Hello World";
+        return String.format("Hello  %s!", test);
     }
 
 }
