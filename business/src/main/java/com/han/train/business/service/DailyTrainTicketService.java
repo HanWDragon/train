5a2b04945ece41e3bda0fc9a3db72bf7bd4eebec
Han <hwl784770@outlook.com>
2024-12-04 22:55:21 +0800

    对每日车票接口使用redis缓存

--- business/src/main/java/com/han/train/business/service/DailyTrainTicketService.java
+++ business/src/main/java/com/han/train/business/service/DailyTrainTicketService.java
@@ -24,6 +24,8 @@ import com.han.train.common.util.SnowUtil;
 import jakarta.annotation.Resource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -60,7 +62,11 @@ public class DailyTrainTicketService {
         }
     }
 
-    // @Cacheable(value = "DailyTrainTicketService.queryList")
+    @CachePut(value = "DailyTrainTicketService.queryList")
+    public PageResp<DailyTrainTicketQueryResp> queryList2(DailyTrainTicketQueryReq req) {
+        return queryList(req);
+    }
+     @Cacheable(value = "DailyTrainTicketService.queryList")
     public PageResp<DailyTrainTicketQueryResp> queryList(DailyTrainTicketQueryReq req) {
         // 常见的缓存过期策略
         // TTL 超时时间
