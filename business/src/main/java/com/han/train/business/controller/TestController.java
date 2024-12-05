198a20ff15b578a7dc282f71c115aa589db699f7
Han <hwl784770@outlook.com>
2024-12-06 03:52:15 +0800

    将Sentinel的信息储存在Nacos

--- business/src/main/java/com/han/train/business/controller/TestController.java
+++ business/src/main/java/com/han/train/business/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.han.train.business.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,8 +10,10 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class TestController {
+    @SentinelResource(value = "hello")
     @GetMapping("/hello")
     public String hello() {
+//        Thread.sleep(500);
         return "Hello World Business";
     }
 
