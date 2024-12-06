4b9f36027ef7225293210dcf9f3cef477d547cf6
Han <hwl784770@outlook.com>
2024-12-07 01:52:33 +0800

    对令牌的获取加锁，5s后自动释放，防止同一个刷票

--- business/src/main/java/com/han/train/business/service/SkTokenService.java
+++ business/src/main/java/com/han/train/business/service/SkTokenService.java
@@ -4,6 +4,7 @@ import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.date.DateTime;
 import cn.hutool.core.date.DateUtil;
 import cn.hutool.core.util.ObjectUtil;
+import com.esotericsoftware.minlog.Log;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import com.han.train.business.domain.SkToken;
@@ -16,12 +17,16 @@ import com.han.train.business.response.SkTokenQueryResp;
 import com.han.train.common.response.PageResp;
 import com.han.train.common.util.SnowUtil;
 import jakarta.annotation.Resource;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class SkTokenService {
@@ -39,6 +44,9 @@ public class SkTokenService {
 
     @Resource
     private SkTokenCustomerMapper skTokenCustomerMapper;
+
+    @Autowired
+    private RedissonClient redissonClient;
 
     /**
      * 初始化
@@ -77,7 +85,24 @@ public class SkTokenService {
     public boolean validSkToken(Date date, String trainCode, Long memberId) {
         LOG.info("会员【{}】获取日期【{}】车次【{}】的令牌开始", memberId, DateUtil.formatDate(date), trainCode);
 
-//         令牌约等于库存，令牌没有了，就不再卖票，不需要再进入购票主流程去判断库存，判断令牌肯定比判断库存效率高
+        // 先获取令牌锁，再校验令牌余量，防止机器人抢票，lockKey就是令牌，用来表示【谁能做什么】的一个凭证
+        // 这里并不需要释放锁，要不然同一个人又可以进来抢票，不需要使用有看门狗，倒计时结束就释放，避免同一个人大量请求
+        RLock lock;
+        String key = DateUtil.formatDate(date) + "-" + trainCode + "-" + memberId;
+        try {
+            lock = redissonClient.getLock(key);
+            boolean tryLock = lock.tryLock(0, 5, TimeUnit.SECONDS); // 不带看门狗，倒计时5s自动释放，避免同一个人刷
+            if (tryLock) {
+                LOG.info("恭喜，抢到令牌锁了！key：{}", key);
+            } else {
+                // 只是没抢到锁，并不知道票抢完了没，所以提示稍候再试
+                LOG.info("很遗憾，没抢到令牌锁！key：{}", key);
+                return false;
+            }
+        } catch (InterruptedException e) {
+            Log.error("获取令牌锁异常", e);
+        }
+//      令牌约等于库存，令牌没有了，就不再卖票，不需要再进入购票主流程去判断库存，判断令牌肯定比判断库存效率高
         int updateCount = skTokenCustomerMapper.decrease(date, trainCode, 1);
         return updateCount > 0;
     }
